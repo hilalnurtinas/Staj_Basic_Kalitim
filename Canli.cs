e394921939f38dae9d7eb1e0edf5b64c8ccb7d58
Hilal Nur TINAS <hilalnurtinass@gmail.com>
2023-07-20 22:28:37 +0300

    Proje dosyası ekle.

---
+++ Canli.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicKalıtım
+{
+    internal class Canli
+    {
+        public double boy;
+        public double agirlik;
+        
+
+        public Canli(double boy, double agirlik)
+        {
+            this.boy = boy;
+            this.agirlik = agirlik;
+        }
+
+        public virtual void Konus()
+        {
+            Console.WriteLine("Ben konusamam!!!");
+        }
+    }
+
+    class Kedi: Canli
+    {
+        public string tur;
+        public Kedi(string tur, int boy, int agirlik): base(boy, agirlik)
+        //base: ile yukarıda tanımlanan double boy ve agirlik'i alıyor(boy ve ağırlıgı kalıtıldıgı sınıftan alıyor)
+        {
+            this.tur = tur;
+        }   
+
+        public override void Konus()
+        {
+            Console.WriteLine("Ben bir kediyim.");    
+        }
+    }
+
+    class Koyun: Canli
+    {
+        public string tur;
+        
+        public Koyun(string tur, int boy, int agirlik) : base(boy, agirlik)
+        {
+            this.tur = tur;
+        }
+
+        public override void Konus()
+        {
+            base.Konus();
+            //base.konus dediğimizde kalıtım aldıgı sınıftaki konus fonksiyonuna gider ve "Ben konusamam!!!" yazar
+        }
+    }
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BasicKalıtım
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Canli canli1 = new Canli(10, 30);
+            Canli canli2 = new Kedi("Van", 20, 10);
+            Kedi kedi1 = new Kedi("Van", 5, 10);
+            Koyun koyun1 = new Koyun("Ankara", 50, 65);
+
+            canli1 = kedi1;
+            canli1.Konus();
+
+            canli1 = koyun1;
+            canli1.Konus();
+
+            canli2.Konus();
+
+        }
+    }
+}
